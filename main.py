7f8173ac374be2d582ac4fc372b2b495cd8b576a
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-12 22:45:11 -0400

    polars main.py

--- main.py
+++ main.py
@@ -1,48 +1,57 @@
-import pandas as pd
-import numpy as np
-import seaborn as sns
+import polars as pl
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-
-
-#Loading Dataset
-wdi = pd.read_csv(
+# Loading Dataset
+wdi = pl.read_csv(
     "https://media.githubusercontent.com/"
     "media/nickeubank/MIDS_Data/"
     "master/World_Development_Indicators/wdi_small_tidy_2015.csv"
 )
 
-#Taking the log of predictor variable(Gdp Per Capita) to determine relationship with outcome variable(Infant Mortality Rate)
-wdi["Log GDP Per Capita"] = np.log(wdi["GDP per capita (constant 2010 US$)"])
 
-#Visualizing the data representation of the relationship between the two variables. 
-chart = (sns.scatterplot(wdi, x= "Log GDP Per Capita", y="Mortality rate, under-5 (per 1,000 live births)")
-         .set(title = "Analysis of Log Gdp Per Capita and Infant Mortality Rate")
+# Function created to understand the descriptive statistices for the dataset of WorldBank Indicators
+def computation(dataframe):
+    gdp_column = dataframe["GDP per capita (constant 2010 US$)"]
+    average = gdp_column.mean()
+    medium = gdp_column.median()
+    std_dev = gdp_column.std()
+
+    # Round the values to the desired number of decimal places
+    average = round(average, 10)
+    medium = round(medium, 10)
+    std_dev = round(std_dev, 2)
+
+    return (average, medium, std_dev)
+
+
+# Calling the function
+average, medium, std_dev = computation(wdi)
+
+# Visualizing the data representation of the relationship between the two variables.
+plt.figure(figsize=(10, 6))
+plt.scatter(
+    wdi["GDP per capita (constant 2010 US$)"],
+    wdi["Mortality rate, under-5 (per 1,000 live births)"],
+    alpha=0.7,
 )
 
-#Display of the chart
+# text on the plot
+text = f"Mean Gdp Per Capita:{average}\nMedian Gdp Per Capita: {medium}\nStd Dev Gdp Per Capita: {std_dev}"
+plt.text(
+    wdi["GDP per capita (constant 2010 US$)"].max() - 0.5,
+    wdi["Mortality rate, under-5 (per 1,000 live births)"].max() - 20,
+    text,
+    bbox=dict(facecolor="white", alpha=0.7),
+    horizontalalignment="right",
+    verticalalignment="top",
+)
+# labeling and title
+plt.xlabel("GDP per capita (constant 2010 US$)")
+plt.ylabel("Mortality rate, under-5 (per 1,000 live births)")
+plt.title = "Analysis of Log Gdp Per Capita and Infant Mortality Rate"
+
+
+# Display of the chart
 plt.show()
-
-#Function created to understand the descriptive statistices for the dataset of WorldBank Indicators
-def computation (dataframe):
-    average = dataframe["Log GDP Per Capita"].mean()
-    average = round(average,10)
-    medium = dataframe["Log GDP Per Capita"].median()
-    medium = round(medium,10)
-    std_dev = dataframe["Log GDP Per Capita"].std()
-    std_dev = round(std_dev,2)
-    return (average,medium,std_dev)
-
-#Calling the function
-computation(wdi)
-
-
-
-
-
-
-
-
-
-
