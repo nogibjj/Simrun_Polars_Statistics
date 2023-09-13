859919b42a739bc9f316b1ac62df37069bcc06b9
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-09-12 22:58:04 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,38 +1,35 @@
-from main import computation
+from week3 import computation
 
-import pandas as pd
+import polars as pl
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
-    
+
 
 def test_computation():
-    wdi = pd.read_csv(
-    "https://media.githubusercontent.com/"
-    "media/nickeubank/MIDS_Data/"
-    "master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+    wdi = pl.read_csv(
+        "https://media.githubusercontent.com/"
+        "media/nickeubank/MIDS_Data/"
+        "master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+    )
 
-    wdi["Log GDP Per Capita"] = np.log(wdi["GDP per capita (constant 2010 US$)"])
-    wdi.dropna(subset=["Log GDP Per Capita"], inplace=True)
     result = computation(wdi)
-    expected_mean = sum(wdi["Log GDP Per Capita"])/len(wdi["Log GDP Per Capita"])
-    expected_mean = round(expected_mean,10)
-    sort_wdi = sorted(wdi["Log GDP Per Capita"])
+    expected_mean = sum(wdi["GDP per capita (constant 2010 US$)"]) / len(
+        wdi["GDP per capita (constant 2010 US$)"]
+    )
+    expected_mean = round(expected_mean, 10)
+    sort_wdi = sorted(wdi["GDP per capita (constant 2010 US$)"])
     n = len(sort_wdi)
     if n % 2 == 0:
-      expected_median = (sort_wdi[n //2 - 1] + sort_wdi[n //2]) / 2
-      expected_median = round(expected_median,10)
+        expected_median = (sort_wdi[n // 2 - 1] + sort_wdi[n // 2]) / 2
+        expected_median = round(expected_median, 10)
     else:
-      expected_median = sort_wdi[n//2]
-      expected_median = round(expected_median,10)
-   
+        expected_median = sort_wdi[n // 2]
+        expected_median = round(expected_median, 10)
+
     expected_standard_dev = 1.47
-    
+
     assert result == (expected_mean, expected_median, expected_standard_dev)
 
 
-
-
 if __name__ == "__main__":
-    pytest.main()
     test_computation()
